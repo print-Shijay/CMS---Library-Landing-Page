87e874769a0fb6e49bae5376a0c53bae7c5aae37
Marc Ciejay Macaraig <macaraigmarcciejay@gmail.com>
2025-12-23 17:28:15 +0800

    Fix the authentication message

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -157,7 +157,7 @@
                     <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
                 </div>
                 <div class="modal-body">
-                    <p>You don’t have permission to access this section.</p>
+                    <p>You don’t have permission to access this section!</p>
                 </div>
                 <div class="modal-footer">
                     <button class="btn btn-secondary" data-bs-dismiss="modal">
